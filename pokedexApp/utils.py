3d4b2e9e6f248f80f4958659b54183d0b93de319
walid <walidouchaou>
2022-11-26 18:41:11 +0100

    first

---
+++ pokedexApp/utils.py
@@ -0,0 +1,156 @@
+import requests
+
+
+def get_pokemon(url_pokemon: str) -> dict:
+    response = requests.get(url_pokemon)
+    pokemon = response.json()
+    specie = get_pokemon_specie(pokemon['species']['url'])
+    name = specie['name']
+    img = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon['id']}.png"
+
+    types = [types['type']['name'].capitalize() for types in pokemon['types']]
+
+    height = pokemon['height'] / 10
+    weight = pokemon['weight'] / 10
+
+    pokemon['types_string'] = ' | '.join(types)
+    pokemon['height_mts'] = height
+    pokemon['weight_kg'] = weight
+    pokemon['img'] = img
+    pokemon['name'] = name.capitalize()
+    return pokemon
+
+
+def get_all_pokemons(amount_pokemons: int) -> list:
+    counter = 0
+    limit = amount_pokemons if amount_pokemons <= 898 else 898
+    url = f"https://pokeapi.co/api/v2/pokemon/?limit={limit}"
+    response = requests.get(url)
+    jsonObject = response.json()
+    pokemonsList = jsonObject['results']
+
+    for pokemon in pokemonsList:
+        counter += 1
+        img = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{counter}.png"
+        pokemon['id'] = f"{counter}"
+        pokemon['img'] = img
+    return pokemonsList
+
+
+def term_checker(pokemon: dict, term: str, array_accumulator: list):
+    if term.lower() in pokemon['name'] or term.lower() in pokemon['id']:
+        array_accumulator.append(pokemon)
+
+
+def get_pokemon_specie(url_specie: str) -> dict:
+    response = requests.get(url_specie)
+    specie = response.json()
+    return specie
+
+
+def get_pokemon_description(list_of_descriptions: list) -> str:
+    for description in list_of_descriptions:
+        if description['language']['name'] == 'en':
+            pokemon_description = description['flavor_text']
+            break
+
+    pokemon_description = pokemon_description.replace('\n', ' ')
+    pokemon_description = pokemon_description.replace('\f', ' ')
+    pokemon_description = pokemon_description.replace('POKéMON', 'pokemon')
+    return pokemon_description
+
+
+def get_pokemon_habitat(specie: dict) -> str:
+    habitat = 'Uninformed' if not specie['habitat'] else specie['habitat']['name'].capitalize()
+    return habitat
+
+
+def get_pokemon_growth_rate(specie: dict) -> str:
+    growth_rate = 'Uninformed' if not specie['growth_rate'] else specie['growth_rate']['name'].capitalize()
+    return growth_rate
+
+
+def get_pokemon_shape(specie: dict) -> str:
+    shape = 'Uninformed' if not specie['shape'] else specie['shape']['name'].capitalize()
+    return shape
+
+
+def get_pokemon_type(url_type: str) -> dict:
+    response = requests.get(url_type)
+    type = response.json()
+
+    weakness = type['damage_relations']['double_damage_from']
+    strenghts = type['damage_relations']['double_damage_to']
+    list_weakness = get_pokemon_weakness_or_strenghts(weakness)
+    list_strenghts = get_pokemon_weakness_or_strenghts(strenghts)
+
+    type['list_weakness'] = list_weakness
+    type['list_strenghts'] = list_strenghts
+    return type
+
+
+def get_pokemon_weakness_or_strenghts(list: list) -> list:
+    list_items = [item['name'] for item in list]
+    return list_items
+
+
+def split_lists_in_list(list_values: list) -> list:
+    new_list = []
+    for array in list_values:
+        for item in array:
+            new_list.append(item)
+    return new_list
+
+
+def remove_duplicates(list_values: list) -> list:
+    new_list = list(dict.fromkeys(list_values))
+    return new_list
+
+
+def get_pokemon_evolutions(specie: dict) -> dict:
+    evolutions_pokemon = {}
+    url_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    response = requests.get(specie['evolution_chain']['url'])
+    evolutions = response.json()
+
+    evolution_one = get_pokemon_specie(evolutions['chain']['species']['url'])
+    evolutions_pokemon['evolution_one'] = get_pokemon(f"{url_pokemon}{evolution_one['id']}")
+    evolutions_pokemon['more_than_two_evolutions'] = False
+
+    pokemon_evolves = True if len(evolutions['chain']['evolves_to']) > 0 else False
+
+    if pokemon_evolves:
+        more_than_two_evolutions = True if len(evolutions['chain']['evolves_to'][0]['evolves_to']) > 0 else False
+
+        evolution_two = get_pokemon_specie(evolutions['chain']['evolves_to'][0]['species']['url'])
+        evolutions_pokemon['evolves'] = True
+        evolutions_pokemon['evolution_two'] = get_pokemon(f"{url_pokemon}{evolution_two['id']}")
+
+        if more_than_two_evolutions:
+            evolutions_pokemon['more_than_two_evolutions'] = True
+            evolution_three = get_pokemon_specie(
+                evolutions['chain']['evolves_to'][0]['evolves_to'][0]['species']['url'])
+            evolutions_pokemon['evolution_three'] = get_pokemon(f"{url_pokemon}{evolution_three['id']}")
+
+    else:
+        evolutions_pokemon['evolves'] = False
+    return evolutions_pokemon
+
+
+def get_pokemon_by_type(id_type: str) -> dict:
+    if int(id_type) < 1 or int(id_type) > 18:
+        print('************ IL NEXISTE PAS DE TEL TYPE')
+        return
+
+    url_type = f"https://pokeapi.co/api/v2/type/{id_type}"
+    response = requests.get(url_type)
+    type = response.json()
+    pokemon_list = type['pokemon']
+    type_name = type['name']
+
+    context = {
+        'pokemons_list': pokemon_list,
+        'type_name': type_name
+    }
+    return context
+
---
+++ pokedexApp/views.py
@@ -0,0 +1,114 @@
+from .utils import *
+from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator
+
+
+class HomeView(TemplateView):
+    template_name ="pokedexApp\index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemonsList = get_all_pokemons(898)
+        paginate_by = 12
+
+        paginator = Paginator(pokemonsList, paginate_by)
+        page = self.request.GET.get('page')
+        pokemonsList = paginator.get_page(page)
+
+        pokemons = [get_pokemon(pokemon['url']) for pokemon in pokemonsList]
+
+        context['pokemonsList'] = pokemonsList
+        context['pokemons'] = pokemons
+        return context
+
+
+class PokemonView(TemplateView):
+    template_name = 'pokedexApp/pokemon.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemonID = self.request.GET.get('pokemonID')
+        pokemon = get_pokemon(f"https://pokeapi.co/api/v2/pokemon/{pokemonID}")
+        specie = get_pokemon_specie(pokemon['species']['url'])
+        weakness_list = []
+        strenghts_list = []
+
+        for types in pokemon['types']:
+            url = types['type']['url']
+            type = get_pokemon_type(url)
+            weakness_list.append(type['list_weakness'])
+            strenghts_list.append(type['list_strenghts'])
+
+        weakness_list = split_lists_in_list(weakness_list)
+        strenghts_list = split_lists_in_list(strenghts_list)
+
+        pokemon_evolutions = get_pokemon_evolutions(specie)
+        evolution_one = pokemon_evolutions['evolution_one']
+
+        if pokemon_evolutions['evolves']:
+            context['pokemon_evolution_two'] = pokemon_evolutions['evolution_two']
+            if pokemon_evolutions['more_than_two_evolutions']:
+                context['pokemon_evolution_three'] = pokemon_evolutions['evolution_three']
+
+        description_list = specie['flavor_text_entries']
+        description_formated = get_pokemon_description(description_list)
+
+        pokemon['description'] = description_formated
+        pokemon['habitat'] = get_pokemon_habitat(specie)
+        pokemon['growth_rate'] = get_pokemon_growth_rate(specie)
+        pokemon['shape'] = get_pokemon_shape(specie)
+
+        context['weakness_list'] = remove_duplicates(weakness_list)
+        context['strenghts_list'] = remove_duplicates(strenghts_list)
+        context['pokemon_evolution_one'] = evolution_one
+        context['pokemon_evolves'] = pokemon_evolutions['evolves']
+        context['evolves_three_times'] = pokemon_evolutions['more_than_two_evolutions']
+        context['pokemonID'] = pokemonID
+        context['pokemon'] = pokemon
+        context['specie'] = specie
+        return context
+class SearchView(TemplateView):
+  template_name = 'pokedexApp/pokemon_search.html'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    term = self.request.GET.get('term')
+    paginated_by = 12
+
+    allPokemons = get_all_pokemons(898)
+    pokemonsList = []
+    for pokemon in allPokemons:
+      term_checker(pokemon, term, pokemonsList)
+
+
+    paginator = Paginator(pokemonsList, paginated_by)
+    page = self.request.GET.get('page')
+    pokemonsList = paginator.get_page(page)
+
+    pokemons = [get_pokemon(pokemon['url']) for pokemon in pokemonsList]
+
+    context['pokemons'] = pokemons
+    context['pokemonsList'] = pokemonsList
+
+    return context
+class TypeView(TemplateView):
+  template_name = 'pokedexApp/pokemon_search_type.html'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    type = self.request.GET.get('id')
+    paginated_by = 12
+
+    pokemons_by_type = get_pokemon_by_type(type)
+    pokemons_list = pokemons_by_type['pokemons_list']
+
+    paginator = Paginator(pokemons_list, paginated_by)
+    page = self.request.GET.get('page')
+    pokemons_list = paginator.get_page(page)
+
+    pokemons = [get_pokemon(pokemon['pokemon']['url']) for pokemon in pokemons_list]
+
+    context['type_name'] = pokemons_by_type['type_name']
+    context['pokemons'] = pokemons
+    context['pokemonsList'] = pokemons_list
+    return context
